7b63134e1367883fe27432ab6a7ca3758711c48c
Ibrahimnasir34 <112743846+Ibrahimnasir34@users.noreply.github.com>
2024-02-23 01:09:19 +0500

    Add files via upload

---
+++ Sailing_Downstream/main.py
@@ -0,0 +1,14 @@
+def process_list(input_list):
+    if len(input_list) % 10 != 0:
+        raise ValueError("Input list length must be a multiple of 10")
+
+    output_list = [num for idx, num in enumerate(input_list) if (idx + 1) % 2 != 0 and (idx + 1) % 3 != 0]
+    return output_list
+
+if __name__ == "__main__":
+    try:
+        input_list = [int(x) for x in input("Enter a list of integers separated by spaces: ").split()]
+        result = process_list(input_list)
+        print("Processed list:", result)
+    except ValueError as e:
+        print("Error:", e)
